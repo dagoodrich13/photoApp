eb210abbd9e0d7494f7c7970ee3c7196e228295f
David Goodrich <dagoodrich13@gmail.com>
2018-02-25 20:19:21 -0700

    most recent features

--- javascript.js
+++ javascript.js
@@ -1,3 +1,27 @@
+var app = angular.module('photoApp',['ngMaterial']);
+app.controller('imageController',['$scope', function($scope){
+  $scope.files=[];
+  $scope.myDrop = function(event){
+    event.preventDefault();
+    for(var i=0; i<event.dataTransfer.files.length; i++){
+      $scope.files.push(event.dataTransfer.files[i].path);
+    }
+    console.log($scope.files);
+    $scope.$apply();
+  }
+}]);
+app.directive("dgDraggable",function(){
+  return{
+    scope:{
+      myDrop: '&dgDrop'
+    },
+    link: function(scope, element, attrs){
+      element.on('drop', function(evt){
+        scope.myDrop({event:evt});
+      })
+    }
+  }
+});
 window.$ = window.jQuery = require('jquery');
 window.ondragover = function(event){
   event.preventDefault();
@@ -7,13 +31,8 @@ window.ondrop = function(event){
   event.preventDefault();
   return false;
 }
-var files = [];
-var dropped = function(event){
-  event.preventDefault();
-  console.log(event);
-  for(var i=0; i<event.dataTransfer.files.length; i++){
-    files.push(event.dataTransfer.files[i].path);
-  }
-}
 $(document).ready(function(){
+  $('#submit-button').click(function(){
+
+  })
 })
